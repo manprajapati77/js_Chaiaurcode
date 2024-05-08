cb567c375881295fa6a127643d29757f3563f6f7
manprajapati77 <manprajapati77@gmail.com>
2024-05-08 23:35:02 +0530

    Some research on variables

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 123
+let password = "mannubhai"
+var email = "321"
+gmail = "raam"
+
+// accountId = 321
+
+password = "ankubhai"
+email = "kyu batau"
+gmail = "Radhe Radhe"
+//console.log(accountId)
+
+
+/* 
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+console.table([accountId, password, email, gmail])
