4b946f7a405ccc03d3c6b24a70954000e744e075
manprajapati77 <manprajapati77@gmail.com>
2024-05-09 22:19:29 +0530

    complete the note of dataTypes Part 1

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict" // treat all JS code as newer version  Note : but also Even without it, js is automaticly treat code as new version of js
+
+//alert(3 + 3) we are using nodejs not browser thats why this is throw an error 
+
+let name = "man"
+let number = 123
+let isAccount = true
+let man;
+let ram = null
+
+//Primitive data types
+/*null
+number
+symble
+string
+boolean
+bigint
+undefined*/
+
+// type of null is object
