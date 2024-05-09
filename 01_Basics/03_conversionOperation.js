b8111f74f4f644e2a33c7e6132ed6bd51dcb6458
manprajapati77 <manprajapati77@gmail.com>
2024-05-09 23:01:33 +0530

    isme mene conversionOperation ko karna sikha jisme hum value ko change karte he to vo kesa beheive karti he

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+/*let strTo = "123m"
+let strToNum = Number(strTo)
+console.log(typeof strToNum) */
+// 123m => Output : NaN, But type of this mix value is number
+
+
+
+/*
+let strTo = "123"
+let strToNum = Number(strTo)
+console.log(typeof strToNum)
+*/
+// 123 => Output : 123, type of this value is number
+
+
+/*let booleanTo = true
+let booleanToNum = Number(booleanTo)
+console.log(booleanToNum)
+console.log(typeof booleanToNum)*/
+// true => Output : 1, type of this value is number
+// false => Output : 0, type of this value is number
+
+let strTo = "asdf"
+let strToboolean = Boolean(strTo)
+console.log(strToboolean)
+console.log(typeof strToboolean)
+// 1 => Output : true, type of this value is boolean
+// 0 => Output : 0, type of this value is boolean
+// "" => Output : false, type of this value is boolean
+// "sometext" => Output : false, type of this value is boolean
