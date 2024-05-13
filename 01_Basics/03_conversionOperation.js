6bc89dd3065253096f3599532f97652dce1ed97f
manprajapati77 <manprajapati77@gmail.com>
2024-05-13 23:08:29 +0530

    in this portion we learn how to use conversion Operation and some rules about conversion Operation

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -20,11 +20,30 @@ console.log(typeof booleanToNum)*/
 // true => Output : 1, type of this value is number
 // false => Output : 0, type of this value is number
 
+
+/*
 let strTo = "asdf"
 let strToboolean = Boolean(strTo)
 console.log(strToboolean)
 console.log(typeof strToboolean)
+*/
+
+
 // 1 => Output : true, type of this value is boolean
 // 0 => Output : 0, type of this value is boolean
 // "" => Output : false, type of this value is boolean
 // "sometext" => Output : false, type of this value is boolean
+
+
+
+/*************** Operations ***************** */
+/*
+let value = 3
+let negval = -value
+console.log(negval)
+*/
+
+let name = "man"
+let surname = " Prajapati"
+let fulln = name + surname
+console.log(fulln)
