b8e0cc793fc2ec82f64e4c6bf124c5c94eae1ac7
manprajapati77 <manprajapati77@gmail.com>
2024-05-14 22:25:43 +0530

    add some changes

---
+++ 01_Basics/datatypes.summery.js
@@ -0,0 +1,17 @@
+// There are 2 type of data types primitive data type and reference (Non Primitive) data type
+
+//Primitive Data type
+//null, number, symbol, string, boolean, bigint, undefined
+
+const num = 123
+const str = "man"
+const thisisbigint = 15616156n
+const thisisboolean = false
+const thisisnull = null
+let thisisundefined;
+
+
+const thisissymbol = Symbol('man')
+const thisisanothersymbol = Symbol('man')
+
+console.log(thisissymbol === thisisanothersymbol) //these are not same the output is false
