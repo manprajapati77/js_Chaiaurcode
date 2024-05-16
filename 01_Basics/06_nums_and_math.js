ea824f16f9d53bf71934ff5ccfb62120d8a98e92
manprajapati77 <manprajapati77@gmail.com>
2024-05-16 22:11:03 +0530

    complete number methods

---
+++ 01_Basics/06_nums_and_math.js
@@ -0,0 +1,19 @@
+const score = 100
+console.log(score)
+
+const balance = new Number(400)
+console.log(balance)
+//This will absolutly a number
+
+console.log(balance.toString().length)
+console.log(balance.toFixed(2))
+
+const othernum = 123.1589
+console.log(othernum.toPrecision(4))
+
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString()) //toLocalString() se numbers me dollar ke hisab se coma lag jate he taki hume padne me asani ho
+console.log(hundreds.toLocaleString('en-IN')) //toLocalString('en-IN') is method se numbers me rupee ke hisab se coma lag jate he taki hume padne me asani ho is method ke bare me MDN me pad sakte he
+
+// *************************** MATH"S ***********************************
