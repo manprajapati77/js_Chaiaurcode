03d0ad95b141c312672d069cf4baf6745c121b4f
manprajapati77 <manprajapati77@gmail.com>
2024-05-17 23:24:44 +0530

    learn about array methods

---
+++ 02_Basics/01_array.js
@@ -0,0 +1,21 @@
+let myarr = [1, 2, "nab"]
+// let myarr2 = [01, 02, 03]
+console.log(myarr[1])
+
+
+// const myarr2 = new Array(1, 2, "nab")
+// console.log(myarr2)
+
+// myarr.push(10, 1516, "man")
+// myarr.pop()
+// console.log(myarr)
+
+//myarr[1] = "replace"
+
+//let myarr2 = myarr.join(" ")
+// console.log(myarr2)
+//myarr.unshift(1, 5, 6, 00)
+
+
+
+console.log(myarr)
