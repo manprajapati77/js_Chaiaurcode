fed30342df3d75542f48bfe19056282588bb7df9
manprajapati77 <manprajapati77@gmail.com>
2024-05-18 00:24:19 +0530

    kar do diya ab jaan lega kya

--- 02_Basics/02_array.js
+++ 02_Basics/02_array.js
@@ -1,3 +1,24 @@
-let marble = [1, 2, 3]
-let dc = [10, 20, 30]
-marble.push(dc)
+let marble = [1, 2, 3];
+let dc = [10, 20, 30];
+//marble.push(dc)
+
+//let newhero = marble.concat(dc)
+
+//let newhero2 = [...marble, ...dc] // this is new or famous way to join 2 or more arrays.
+//console.log(newhero2)
+
+// let mixarr = [1, 2, 3, ["a", "b", "c", [1, "anns", "ss"]]]
+// let solvemixarr = mixarr.flat(Infinity)
+// console.log(mixarr)
+// console.log(solvemixarr)
+
+console.log(Array.isArray("man")) // for check give type is array or not
+
+console.log(Array.from("hites")) // convert into array
+
+console.log(Array.from({name: "man"})) // give empty array search for ans on google
+let scr1 = 100
+let scr2 = 200
+let scr3 = 300
+
+console.log(Array.of(scr1, scr2, scr3))
