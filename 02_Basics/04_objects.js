e7d8ba6d5f72defa3dcacd5401d079071dbe1584
manprajapati77 <manprajapati77@gmail.com>
2024-05-19 15:43:34 +0530

    object and some api knowledge clear

--- 02_Basics/04_objects.js
+++ 02_Basics/04_objects.js
@@ -38,4 +38,29 @@ const obj3 = {...obj1, ...obj2}
 
 //if we want to convert object key or value to array we have to use Object.entries() this method give us set of key and value in the form of arrays in array
 
-console.log(Object.entries(tinderUser))
+//console.log(Object.entries(tinderUser))
+
+
+//Objects ko access karne ke liye bahut bada naam likhna padta he isko short kiya ja sakta destructuring ki help se
+
+//destructuring
+
+const man = {
+    course: "jscode",
+    price: 99,
+    teacherName: "hitesh"
+}
+//console.log(man.teacherName)
+//agar ab isme se mujhe teacher ko acess karna he to mujhe bada sa code likhna padega isko solve karne ke liye me destructuring ki help lunga
+
+//const {teacherName} = man //ise hi destructuring kahte he
+//agar mujhe ab ye key bada lag raha he to me uske naam ko chote naam se change kar sakta hu
+const {teacherName: Nam} = man
+
+//console.log(Nam)
+
+// {
+//     "name": "man",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
