dfb0ce7b7a883cb8151b5ec1844fe4ca34e5f9f2
manprajapati77 <manprajapati77@gmail.com>
2024-06-04 22:23:29 +0530

    add remining files

--- Project 2/Project 2.1/script.js
+++ Project 2/Project 2.1/script.js
@@ -11,11 +11,18 @@ const randomColor = function(){
 function changeColor(){
     document.body.style.backgroundColor = randomColor()
 }
+let startchange
 function startchangingcolor(){
-    let startchange = setInterval(changeColor, 500000)
+    if(!startchange){
+        //!startchange ka mtlb null hi he ise startchange == null bhi likh sakte the
+        startchange = setInterval(changeColor, 1000)
+    }
 }
 function stopchangingcolor(){
     clearInterval(startchange)
+    startchange = null
+    //we put null value taki ye clear interval ke baad variable khali ho jaye jisse memory jyada nahi fill hogi or ye ek better tarika he code likhne ka 
+    //sath hi ye karne per hume if condition add karna pada kyuki yadi hum variable ko pura khali kar dete he to value thik se work nahi kar rhi thi !startchange ka mtlb null hi he
 }
 
 
@@ -23,11 +30,3 @@ document.querySelector('#start').addEventListener('click', startchangingcolor)
 document.querySelector('#stop').addEventListener('click', stopchangingcolor)
 
 
-/*
-function man(){
-    console.log("free")
-}
-let man = function (){
-    console.log("no free")
-}
-*/
