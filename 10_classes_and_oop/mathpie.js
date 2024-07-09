6aaa6984e7ca9a36edc1d531ce18a496c7de6ee8
manprajapati77 <manprajapati77@gmail.com>
2024-07-09 21:56:33 +0530

    add all changes

---
+++ 10_classes_and_oop/mathpie.js
@@ -0,0 +1,16 @@
+// const descriptor = Object.getOwnPropertyDescriptor(Math, "PI")
+// console.log(descriptor);
+
+
+// console.log(Math.PI);
+// Math.PI = 5
+// console.log(Math.PI)
+
+const chai = {
+    name: "gigerchai",
+    price: 500,
+    isAvailable: true
+}
+console.log(chai);
+console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+//2 perameter lete he first kiske ander se lena he aur uske ander se kya lena he
